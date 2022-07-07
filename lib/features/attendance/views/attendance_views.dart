374adf5671d264be7da8ba0ed8b815c55c3babc5
rohan165-boop <rohanchy165@gmail.com>
2022-07-07 08:49:58 +0545

    comments

--- lib/features/attendance/views/attendance_views.dart
+++ lib/features/attendance/views/attendance_views.dart
@@ -1,5 +1,4 @@
-import 'dart:developer';
-
+import 'dart:io';
 import 'package:classapp/helpers/image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,6 +11,7 @@ class AttendanceView extends StatefulWidget {
 }
 
 class _AttendanceViewState extends State<AttendanceView> {
+  XFile? _pickedImage;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,49 +21,81 @@ class _AttendanceViewState extends State<AttendanceView> {
       ),
 
       body: Center(
-        child:  ElevatedButton(onPressed: () {
-                showModalBottomSheet(context: context, 
-                backgroundColor: Colors.grey.shade400,
-                shape:const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
-                  )
-                ),
-                builder: (context) {
-                  return SizedBox(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                       ListTile(
-                        onTap: () async{ 
-                         ImagePickerHelper().pickImage(ImageSource.gallery);
-                        },
-                        leading:const Icon(Icons.photo, size: 50,),
-                        title:const  Text("Upload Image",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold
-                        ),),
-                       ),
+        child: ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 160, right: 160),
+              child: ElevatedButton(onPressed: () {
+                      showModalBottomSheet(context: context, 
+                      backgroundColor: Colors.grey.shade400,
+                      shape:const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        )
+                      ),
+                      builder: (context) {
+                        return SizedBox(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                             ListTile(
+                              onTap: () async{ 
+                                Navigator.pop(context);
+                               _pickedImage = await ImagePickerHelper().pickImage(ImageSource.gallery);
+                               setState(() {});
+                              },
+                              leading:const Icon(Icons.photo, size: 50,),
+                              title:const  Text("Upload Image",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold
+                              ),),
+                             ),
 
-                       ListTile(
-                        onTap: () async{
-                          ImagePickerHelper().pickImage(ImageSource.camera);
-                        },
-                         leading:const Icon(Icons.camera_alt, size: 50,),
-                        title:const  Text("Camera",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold
-                        ),),
-                       ),
-                      ],
+                             ListTile(
+                              onTap: () async{
+                                Navigator.pop(context);
+                                _pickedImage = await ImagePickerHelper().pickImage(ImageSource.camera);
+                                setState(() {});
+                              },
+                               leading:const Icon(Icons.camera_alt, size: 50,),
+                              title:const  Text("Camera",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold
+                              ),),
+                             ),
+                            ],
+                          ),
+                        );
+                      });
+                    }, 
+                    child: const Icon(Icons.more_horiz, 
+                    size: 30,),
                     ),
-                  );
-                });
-              }, 
-              child: const Icon(Icons.more_horiz, size: 30,),),
+            ),
+
+                  if (_pickedImage != null)...{
+                    Stack(
+                      alignment: Alignment.topRight,
+                      children: [
+                        Image.file(
+                      File(_pickedImage!.path),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      ),
+                      IconButton(onPressed: () {
+                        _pickedImage = null;
+                        setState(() {});
+                      }, 
+                      icon: const Icon(Icons.cancel, color: Colors.red, size: 40,),),
+                      ],
+                    )
+                  }
+          ],
+        ),
       ),
       
     );
--- lib/helpers/image_picker.dart
+++ lib/helpers/image_picker.dart
@@ -1,10 +1,9 @@
-import 'dart:developer';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerHelper{
-  pickImage( ImageSource source) async{ 
+  Future<XFile?> pickImage( ImageSource source) async{ 
    final ImagePicker _picker = ImagePicker();
-   final image = await _picker.pickImage(source:source);
-   log(image != null ? image.path: "null");
+   final XFile? image = await _picker.pickImage(source:source);
+   return image;
   }
 }
