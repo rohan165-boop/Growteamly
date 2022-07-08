4a5dacb2997778115dd34879530db239ffe3e971
rohan165-boop <rohanchy165@gmail.com>
2022-07-08 08:53:35 +0545

    message

--- lib/features/leaves/views/leaves_views.dart
+++ lib/features/leaves/views/leaves_views.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class LeavesView extends StatefulWidget {
@@ -12,7 +13,59 @@ class _LeavesViewState extends State<LeavesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("leaves"),
+        automaticallyImplyLeading: false,
+        title:const Text("leaves"),
+      ),
+
+      body: Column(
+        children: [
+           Expanded(
+            flex: 4,
+            child: CachedNetworkImage(imageUrl: 
+            "https://machinelearningmastery.com/wp-content/uploads/2017/01/A-Gentle-Introduction-to-the-Random-Walk-for-Times-Series-Forecasting-with-Python.jpg",
+            // height: 300,
+            // width: double.infinity,
+            fit: BoxFit.cover,
+            errorWidget: (context, url,error) => const Icon(Icons.error, size: 50, color: Colors.redAccent,),
+            progressIndicatorBuilder: (context, url, error) => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            ),
+          ),
+          const SizedBox(height: 20,),
+
+          Container(
+           height: 50,
+           decoration: BoxDecoration(
+             color: Colors.grey.shade400,
+             borderRadius: BorderRadius.circular(25),
+             border: Border.all(color: Colors.black)
+           ),
+           
+           child: Padding(
+             padding: const EdgeInsets.only(left: 40, right: 40),
+             child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(25),
+                borderSide: BorderSide(color: Colors.red),),
+                focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(25),
+                borderSide: BorderSide(color: Colors.yellowAccent),),
+                
+              ),
+               
+             ),
+           ),),
+           const SizedBox(height: 20,),
+
+          Expanded(flex: 3, 
+          child: Container(
+            decoration:const BoxDecoration(
+              gradient: LinearGradient(colors:[ Colors.orange, Colors.greenAccent, Colors.pinkAccent  ])
+            ),
+            ),),
+        ],
       ),
       
     );
